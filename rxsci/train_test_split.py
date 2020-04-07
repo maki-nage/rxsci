9cfade0275b181515d5352cc33e60633c2a8908a
Romain Picard <romain.picard@oakbits.com>
2020-04-07 23:51:47 +0200

    add sampling to train_test_split

--- rxsci/train_test_split.py
+++ rxsci/train_test_split.py
@@ -32,19 +32,28 @@ def ref_count():
     return _ref_count
 
 
-def train_test_split(test_ratio):
+def train_test_split(test_ratio, sampling_size=1):
     test_modulus = int(1/test_ratio)
 
     def _train_test_split(source):
         def partition(acc, i):
-            index = 1 if acc is None else acc[0]
+            if acc is None:
+                index = 1
+                sampling = sampling_size
+            else:
+                index = acc[0]
+                sampling = acc[1]
             if test_modulus == 0:
                 is_test = False
             else:
                 is_test = True if index % test_modulus == 0 else False
 
-            print((index+1, i, is_test))
-            return (index+1, i, is_test)
+            sampling -= 1
+            if sampling == 0:
+                index += 1
+                sampling = sampling_size
+
+            return (index, sampling, i, is_test)
 
         published = source.pipe(
             ops.publish(),
@@ -54,13 +63,13 @@ def train_test_split(test_ratio):
         return [
             published.pipe(
                 ops.scan(partition, seed=None),
-                ops.filter(lambda i: i[2] is False),
-                ops.map(lambda i: i[1]),
+                ops.filter(lambda i: i[3] is False),
+                ops.map(lambda i: i[2]),
             ),
             published.pipe(
                 ops.scan(partition, seed=None),
-                ops.filter(lambda i: i[2] is True),
-                ops.map(lambda i: i[1]),
+                ops.filter(lambda i: i[3] is True),
+                ops.map(lambda i: i[2]),
             )
         ]
 
--- tests/test_train_test_split.py
+++ tests/test_train_test_split.py
@@ -3,13 +3,7 @@ from rx.subject import Subject
 import rxsci
 
 
-def test_tran_test_split():
-    dataset = rx.from_([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-
-    train, test = dataset.pipe(
-        rxsci.train_test_split(0.2),
-    )
-
+def base_test(train, test):
     actual_train = []
     actual_test = []
 
@@ -28,5 +22,30 @@ def test_tran_test_split():
         on_error=lambda e: print(e),
     )
 
+    return actual_train, actual_test
+
+
+def test_train_test_split():
+    dataset = rx.from_([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+    train, test = dataset.pipe(
+        rxsci.train_test_split(0.2),
+    )
+
+    actual_train, actual_test = base_test(train, test)
+
     assert actual_train == [0, 1, 2, 3, 5, 6, 7, 8]
     assert actual_test == [4, 9]
+
+
+def test_train_test_split_sampling3():
+    dataset = rx.from_([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+    train, test = dataset.pipe(
+        rxsci.train_test_split(0.5, sampling_size=3),
+    )
+
+    actual_train, actual_test = base_test(train, test)
+
+    assert actual_train == [0, 1, 2, 6, 7, 8]
+    assert actual_test == [3, 4, 5, 9]
