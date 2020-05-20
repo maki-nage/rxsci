f95940c4c6fa540b8460ce7ac4da4fe75291ac3e
Romain Picard <romain.picard@oakbits.com>
2020-05-20 23:49:50 +0200

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ install_requires = [
 
 setup(
     name="rxsci",
-    version='0.1.0',
+    version='0.2.0',
     url='https://github.com/maki-nage/rxsci.git',
     license='MIT',
     description="ReactiveX for data science",
