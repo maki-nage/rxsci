87bfa610c6f1a74369e85ba0a75f10c6437e1b74
Romain Picard <romain.picard@oakbits.com>
2020-05-26 23:48:56 +0200

    line.unframe: improve performances

--- rxsci/framing/line.py
+++ rxsci/framing/line.py
@@ -7,6 +7,32 @@ def unframe():
     ''' unframe a Observable of text to lines
     '''
     def _unframe(source):
+        def on_subscribe(observer, scheduler):
+            acc = ''
+
+            def on_next(i):
+                nonlocal acc
+                lines = i.split('\n')
+                lines[0] = acc + lines[0]
+                acc = lines[-1] or ''
+                for line in lines[0:-1]:
+                    observer.on_next(line)
+
+            return source.subscribe(
+                on_next=on_next,
+                on_completed=observer.on_completed,
+                on_error=observer.on_error
+            )
+        return rx.create(on_subscribe)
+
+    return _unframe
+
+
+"""
+def unframe():
+    ''' unframe a Observable of text to lines
+    '''
+    def _unframe(source):
         def accumulate(acc, i):
             lines = i.split('\n')
             lines[0] = acc[0] + lines[0]
@@ -19,3 +45,4 @@ def unframe():
         )
 
     return _unframe
+"""
