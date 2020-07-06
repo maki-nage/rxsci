bf972b010c68bfe22555a13f8904b2201b7d0892
Romain Picard <romain.picard@oakbits.com>
2020-07-06 15:03:43 +0200

    progress: remove modulus usage, add msg/s metric

--- rxsci/progress.py
+++ rxsci/progress.py
@@ -1,7 +1,8 @@
+from timeit import default_timer as timer
 import rx
 
 
-def progress(name, threshold):
+def progress(name, threshold, measure_throughput=True):
     '''Prints the progress on item processing
 
     Prints the number of items that have been processed every threshold items.
@@ -16,13 +17,28 @@ def progress(name, threshold):
     def _progress(source):
         def on_subscribe(observer, scheduler):
             counter = 0
+            countdown = threshold
+            prev_time = None
 
             def on_next(i):
                 nonlocal counter
+                nonlocal countdown
+                nonlocal prev_time
                 counter += 1
-                if counter % threshold == 0:
-                    print("{} progress: {}".format(name, counter))
-                
+                countdown -= 1
+                if countdown <= 0:
+                    countdown = threshold
+                    if measure_throughput is True:
+                        cur_time = timer()
+                        if prev_time is not None:
+                            mps = threshold // (cur_time - prev_time)
+                        else:
+                            mps = None
+                        prev_time = cur_time
+                        print("{} progress: {} ({} msg/s)".format(name, counter, mps))
+                    else:
+                        print("{} progress: {}".format(name, counter))
+
                 observer.on_next(i)
 
             return source.subscribe(
