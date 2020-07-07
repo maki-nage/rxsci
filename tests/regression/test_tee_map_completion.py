3a1bd0a8ce6d2f12b70b37f1b67f2b1ad0b7ff1e
Romain Picard <romain.picard@oakbits.com>
2020-07-07 11:51:34 +0200

    add regression test on tee_map+math

---
+++ tests/regression/test_tee_map_completion.py
@@ -0,0 +1,22 @@
+import rx
+import rx.operators as ops
+import rxsci as rs
+
+
+def test_completion():
+    data = [1, 2, 3]
+    actual_data = []
+    actual_completed = []
+
+    rx.from_(data).pipe(
+        rs.tee_map(
+            ops.count(),
+            rs.math.sum(reduce=True),
+        )
+    ).subscribe(
+        on_next=actual_data.append,
+        on_completed=lambda: actual_completed.append(True)
+    )
+
+    assert actual_completed == [True]
+    assert actual_data == [(3, 6)]
