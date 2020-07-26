554de7ba74b68b6bc2ea1ec13cc88ff9b83a1b82
Romain Picard <romain.picard@oakbits.com>
2020-07-26 22:29:54 +0200

    mux: add lag

--- rxsci/data/lag.py
+++ rxsci/data/lag.py
@@ -1,6 +1,7 @@
 import sys
 from collections import deque
 import rx
+import rxsci as rs
 
 
 def lag1():
@@ -32,7 +33,33 @@ def lag1():
                 on_completed=observer.on_completed,
                 on_error=observer.on_error,
                 scheduler=scheduler)
-        return rx.create(on_subscribe)
+
+        def on_subscribe_mux(observer, scheduler):
+            last = {}
+
+            def on_next(i):
+                if isinstance(i, rs.OnNextMux):
+                    ii = (last[i.key] if last[i.key] is not None else i.item, i.item)
+                    last[i.key] = i.item
+                    observer.on_next(rs.OnNextMux(i.key, ii))
+                elif isinstance(i, rs.OnCreateMux):
+                    last[i.key] = None
+                    observer.on_next(i)
+                elif isinstance(i, rs.OnCompletedMux) \
+                or isinstance(i, rs.OnErrorMux):
+                    del last[i.key]
+                    observer.on_next(i)
+
+            return source.subscribe(
+                on_next=on_next,
+                on_completed=observer.on_completed,
+                on_error=observer.on_error,
+                scheduler=scheduler)
+
+        if isinstance(source, rs.MuxObservable):
+            return rx.create(on_subscribe_mux)
+        else:
+            return rx.create(on_subscribe)
 
     return _lag1
 
@@ -74,6 +101,40 @@ def lag(size=1):
                 on_error=observer.on_error,
                 scheduler=scheduler,
             )
-        return rx.create(on_subscribe)
+
+        def on_subscribe_mux(observer, scheduler):
+            buffer = {}
+
+            def on_next(i):
+                if isinstance(i, rs.OnNextMux):
+                    q = buffer[i.key]
+                    q.append(i.item)
+                    observer.on_next(rs.OnNextMux(i.key, (q[0], i.item)))
+                    if len(q) > size:
+                        q.popleft()
+
+                elif isinstance(i, rs.OnCreateMux):
+                    buffer[i.key] = deque()
+                    observer.on_next(i)
+                elif isinstance(i, rs.OnCompletedMux) \
+                or isinstance(i, rs.OnErrorMux):
+                    del buffer[i.key]
+                    observer.on_next(i)
+
+            def on_completed():
+                buffer.clear()
+                observer.on_completed()
+
+            return source.subscribe(
+                on_next=on_next,
+                on_completed=on_completed,
+                on_error=observer.on_error,
+                scheduler=scheduler,
+            )
+
+        if isinstance(source, rs.MuxObservable):
+            return rx.create(on_subscribe_mux)
+        else:
+            return rx.create(on_subscribe)
 
     return _lag
--- tests/data/test_lag.py
+++ tests/data/test_lag.py
@@ -25,6 +25,32 @@ def test_lag1():
     assert actual_result == expected_result
 
 
+def test_lag1_mux():
+    source = [
+        rs.OnCreateMux((1 ,None)),
+        rs.OnNextMux((1, None), 1),
+        rs.OnNextMux((1, None), 2),
+        rs.OnNextMux((1, None), 3),
+        rs.OnNextMux((1, None), 4),
+        rs.OnCompletedMux((1, None)),
+    ]
+    actual_result = []
+
+    rx.from_(source).pipe(
+        rs.cast_as_mux_observable(),
+        rs.data.lag1(),
+    ).subscribe(on_next=actual_result.append)
+
+    assert actual_result == [
+        rs.OnCreateMux((1 ,None)),
+        rs.OnNextMux((1, None), (1,1)),
+        rs.OnNextMux((1, None), (1,2)),
+        rs.OnNextMux((1, None), (2,3)),
+        rs.OnNextMux((1, None), (3,4)),
+        rs.OnCompletedMux((1, None)),
+    ]
+
+
 def test_lag():
     source = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     actual_result = []
@@ -45,3 +71,34 @@ def test_lag():
     ).subscribe(on_next=actual_result.append)
 
     assert actual_result == expected_result
+
+
+def test_lag_mux():
+    source = [
+        rs.OnCreateMux((1 ,None)),
+        rs.OnNextMux((1, None), 1),
+        rs.OnNextMux((1, None), 2),
+        rs.OnNextMux((1, None), 3),
+        rs.OnNextMux((1, None), 4),
+        rs.OnNextMux((1, None), 5),
+        rs.OnNextMux((1, None), 6),
+        rs.OnCompletedMux((1, None)),
+    ]
+    actual_result = []
+
+    rx.from_(source).pipe(
+        rs.cast_as_mux_observable(),
+        rs.data.lag(2),
+    ).subscribe(on_next=actual_result.append)
+
+    assert actual_result == [
+        rs.OnCreateMux((1 ,None)),
+        rs.OnNextMux((1, None), (1, 1)),
+        rs.OnNextMux((1, None), (1, 2)),
+        rs.OnNextMux((1, None), (1, 3)),
+        rs.OnNextMux((1, None), (2, 4)),
+        rs.OnNextMux((1, None), (3, 5)),
+        rs.OnNextMux((1, None), (4, 6)),
+        rs.OnCompletedMux((1, None)),
+    ]
+
