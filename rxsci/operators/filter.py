0dcdd0adcbae4972cdae5a14e7955288055d6c4b
Romain Picard <romain.picard@oakbits.com>
2020-07-30 23:25:26 +0200

    mux: add filter

---
+++ rxsci/operators/filter.py
@@ -0,0 +1,59 @@
+import rxsci as rs
+import rx.operators as ops
+
+
+def filter_mux(predicate):
+    def _filter(source):
+        def on_subscribe(observer, scheduler):
+            def on_next(i):
+                if type(i) is rs.OnNextMux:
+                    try:
+                        emit = predicate(i.item)
+                        if emit is True:
+                            observer.on_next(i)
+                    except Exception as e:
+                        observer.on_next(rs.OnErrorMux(i.key, e))
+                else:
+                    observer.on_next(i)
+
+            return source.subscribe(
+                on_next=on_next,
+                on_completed=observer.on_completed,
+                on_error=observer.on_error,
+                scheduler=scheduler
+            )
+
+        return rs.MuxObservable(on_subscribe)
+    return _filter
+
+
+def filter(predicate):
+    """Filters the items of an observable sequence based on a
+    predicate.
+
+    .. marble::
+        :alt: filter
+
+        ----1---2---3---4---|
+        [   filter(i: i>2)  ]
+        ------------3---4---|
+
+    Example:
+        >>> rs.operators.filter(lambda value: value < 10)
+
+    Args:
+        predicate: A function to test each source item for a
+            condition.
+
+    Returns:
+        An observable that emits items from the
+        source observable that satisfy the condition.
+    """
+
+    def _filter(source):
+        if isinstance(source, rs.MuxObservable):
+            return filter_mux(predicate)(source)
+        else:
+            return ops.filter(predicate)(source)
+
+    return _filter
---
+++ tests/operators/test_filter.py
@@ -0,0 +1,44 @@
+import rx
+import rx.operators as ops
+import rxsci as rs
+import rxsci.operators as rsops
+
+
+def test_filter_mux():
+    source = [
+        rs.OnCreateMux((1 ,None)),
+        rs.OnNextMux((1, None), 1),
+        rs.OnCreateMux((2, None)),
+        rs.OnNextMux((2, None), 2),
+        rs.OnNextMux((2, None), 3),
+        rs.OnNextMux((1, None), 4),
+        rs.OnNextMux((1, None), 5),
+        rs.OnCompletedMux((1, None)),
+        rs.OnCompletedMux((2, None)),
+    ]
+    actual_error = []
+    actual_completed = []
+    actual_result = []
+
+    def on_completed():
+        actual_completed.append(True)
+
+    rx.from_(source).pipe(
+        rs.cast_as_mux_observable(),
+        rsops.filter(lambda i: i % 2 == 0),
+    ).subscribe(
+        on_next=actual_result.append,
+        on_completed=on_completed,
+        on_error=actual_error.append,
+    )
+
+    assert actual_error == []
+    assert actual_completed == [True]
+    assert actual_result == [
+        rs.OnCreateMux((1 ,None)),
+        rs.OnCreateMux((2, None)),
+        rs.OnNextMux((2, None), 2),
+        rs.OnNextMux((1, None), 4),
+        rs.OnCompletedMux((1, None)),
+        rs.OnCompletedMux((2, None)),
+    ]
