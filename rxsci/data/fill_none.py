1eec6a7cb2f26f2da37f5e2b80753429527916a6
Romain Picard <romain.picard@oakbits.com>
2020-09-06 22:50:13 +0200

    doc: add fill_none

--- rxsci/data/fill_none.py
+++ rxsci/data/fill_none.py
@@ -1,9 +1,21 @@
-from collections import namedtuple
 import rxsci as rs
 
 
 def fill_none(value):
     '''Replaces None values with value
+
+    .. marble::
+        :alt: fill_none
+
+        -0----none-2----1-----|
+        [    fill_none(42)    ]
+        -0----42---4----1-----|
+
+    Args:
+        value: The value used to replace None values.
+
+    Returns:
+        An observable where None items are replaced with value.
     '''
 
     def __fill_none(i):
