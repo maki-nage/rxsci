45f3fb1cc80de26588590b30a7efe11d1329ab65
Romain Picard <romain.picard@oakbits.com>
2020-09-08 16:20:24 +0200

    last: fix completion on empty MuxObservable

--- rxsci/operators/last.py
+++ rxsci/operators/last.py
@@ -1,11 +1,13 @@
 import rxsci as rs
 import rx.operators as ops
+from rxsci.internal.utils import NotSet
 
 
 def last_mux():
     def _last(source):
         def on_subscribe(observer, scheduler):
             state = []
+
             def on_next(i):
                 if type(i) is rs.OnNextMux:
                     state[i.key[0]] = i.item
@@ -13,16 +15,17 @@ def last_mux():
                     append_count = (i.key[0]+1) - len(state)
                     if append_count > 0:
                         for _ in range(append_count):
-                            state.append(None)                    
+                            state.append(NotSet)
                     observer.on_next(i)
-                elif type(i) is rs.OnCompletedMux:                    
-                    #if i.key[0] in state:
-                    observer.on_next(rs.OnNextMux(i.key, state[i.key[0]]))
+                elif type(i) is rs.OnCompletedMux:
+                    value = state[i.key[0]]
+                    if value is not NotSet:
+                        observer.on_next(rs.OnNextMux(i.key, value))
                     observer.on_next(i)
-                    state[i.key[0]] = None
+                    state[i.key[0]] = NotSet
                 elif type(i) is rs.OnErrorMux:
                     observer.on_next(rs.OnErrorMux(i.key, i.error))
-                    state[i.key[0]] = None
+                    state[i.key[0]] = NotSet
                 else:
                     observer.on_next(TypeError("first: unknow item type: {}".format(type(i))))
 
--- tests/operators/test_last.py
+++ tests/operators/test_last.py
@@ -42,3 +42,34 @@ def test_last_mux():
     ]
 
 
+def test_last_mux_empty():
+    source = [
+        rs.OnCreateMux((1,)),
+        rs.OnCreateMux((2,)),
+        rs.OnCompletedMux((1,)),
+        rs.OnCompletedMux((2,)),
+    ]
+    actual_error = []
+    actual_completed = []
+    actual_result = []
+
+    def on_completed():
+        actual_completed.append(True)
+
+    rx.from_(source).pipe(
+        rs.cast_as_mux_observable(),
+        rs.ops.last(),
+    ).subscribe(
+        on_next=actual_result.append,
+        on_completed=on_completed,
+        on_error=actual_error.append,
+    )
+
+    assert actual_error == []
+    assert actual_completed == [True]
+    assert actual_result == [
+        rs.OnCreateMux((1,)),
+        rs.OnCreateMux((2,)),
+        rs.OnCompletedMux((1,)),
+        rs.OnCompletedMux((2,)),
+    ]
