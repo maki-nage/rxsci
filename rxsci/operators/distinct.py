2bb583b4147d45f893702d9efbe3f4412f7581ea
Romain Picard <romain.picard@oakbits.com>
2020-09-25 14:22:39 +0200

    distinct: move doc to correct level

--- rxsci/operators/distinct.py
+++ rxsci/operators/distinct.py
@@ -4,42 +4,41 @@ import rxsci as rs
 
 
 def distinct(key_mapper=None):
+    """Returns an observable sequence that contains only distinct
+    elements according to the key_mapper. Usage of
+    this operator should be considered carefully due to the maintenance
+    of an internal lookup structure which can grow large.
 
+    .. marble::
+        :alt: distinct
+
+        -0-1-2-1-3-4-2-0---|
+        [    distinct()    ]
+        -0-1-2---3-4-------|
+
+    This operator is similar to the RxPY distinct operator but with much
+    better performance thanks to the usage of sets. Meanwhile this
+    implementation does not allow to specify a comparer: The key must be
+    hashable.
+
+
+    Examples:
+        >>> data = range(200000, 0, -1)
+        >>> rx.from_(data).pipe(
+        >>>     ops.distinct(),
+        >>> )
+
+    Args:
+        key_mapper: [Optional]  A function to compute the comparison
+            key for each element.
+
+    Returns:
+        An operator function that takes an observable source and
+        returns an observable sequence only containing the distinct
+        elements, based on a computed key value, from the source
+        sequence.
+    """
     def _distinct(source):
-        """Returns an observable sequence that contains only distinct
-        elements according to the key_mapper. Usage of
-        this operator should be considered carefully due to the maintenance
-        of an internal lookup structure which can grow large.
-
-        .. marble::
-            :alt: distinct
-
-            -0-1-2-1-3-4-2-0---|
-            [    distinct()    ]
-            -0-1-2---3-4-------|
-
-        This operator is similar to the RxPY distinct operator but with much
-        better performance thanks to the usage of sets. Meanwhile this
-        implementation does not allow to specify a comparer: The key must be
-        hashable.
-
-
-        Examples:
-            >>> data = range(200000, 0, -1)
-            >>> rx.from_(data).pipe(
-            >>>     ops.distinct(),
-            >>> )
-
-        Args:
-            key_mapper: [Optional]  A function to compute the comparison
-                key for each element.
-
-        Returns:
-            An operator function that takes an observable source and
-            returns an observable sequence only containing the distinct
-            elements, based on a computed key value, from the source
-            sequence.
-        """
         mux = True if isinstance(source, rs.MuxObservable) else False
 
         def on_subscribe(observer, scheduler=None):
