07c9b0e4beca6cfeaeca7a302a4e83389fbb50d8
Romain Picard <romain.picard@oakbits.com>
2020-12-15 15:24:23 +0100

    with_latest_from: add docstring

--- rxsci/operators/with_latest_from.py
+++ rxsci/operators/with_latest_from.py
@@ -5,6 +5,31 @@ from rxsci.internal.utils import NotSet
 
 
 def with_latest_from(*sources):
+    """Merges the specified observables into one observable sequence by
+    creating a tuple only when the source observable sequence produces an
+    element. The source observable is subscribed once at least one item has
+    been received on each additional source.
+
+    .. marble::
+        :alt: with_latest_from
+          --1--2----3----4-|
+        --a-------b---c--d---|
+        [ with_latest_from() ]
+        ----1,a-2,a-3,b--4,d-|
+
+    Examples:
+        >>> rs.ops.with_latest_from(obs1
+
+    Source:
+        An Observable.
+
+    Args:
+        sources: Sequence of observables.
+
+    Returns:
+        An observable sequence containing the result of combining
+        elements of the sources into a tuple.
+    """
     def _with_latest_from(parent):
         NO_VALUE = NotSet()
 
@@ -34,6 +59,6 @@ def with_latest_from(*sources):
                 children_subscription = [subscribe_child(i, child) for i, child in enumerate(children)]
 
                 return [parent_subscription] + children_subscription
-            return CompositeDisposable(subscribe_all(parent, *sources))        
+            return CompositeDisposable(subscribe_all(parent, *sources))
         return rx.create(on_subscribe)
     return _with_latest_from
