6c0edd09f15808d26e18003e406a37c352bb6162
Romain Picard <romain.picard@oakbits.com>
2021-01-05 13:49:53 +0100

    starmap: add doc

--- rxsci/operators/starmap.py
+++ rxsci/operators/starmap.py
@@ -3,29 +3,33 @@ import rxsci as rs
 from .map import map as map
 
 
-def map_mux(mapper):
-    def _map(source):
-        def on_subscribe(observer, scheduler):
-            def on_next(i):
-                if type(i) is rs.OnNextMux:
-                    try:
-                        ii = mapper(i.item)
-                        observer.on_next(rs.OnNextMux(i.key, ii))
-                    except Exception as e:
-                        observer.on_next(rs.OnErrorMux(i.key, e))
-                else:
-                    observer.on_next(i)
-
-            return source.subscribe(
-                on_next=on_next,
-                on_completed=observer.on_completed,
-                on_error=observer.on_error,
-                scheduler=scheduler
-            )
-
-        return rs.MuxObservable(on_subscribe)
-    return _map
-
-
 def starmap(mapper):
+    """
+    Unpacks arguments grouped as tuple items of an observable
+    and return an observable of values by invoking
+    the mapper function with star applied unpacked items as
+    positional arguments.
+
+    .. marble::
+        :alt: starmap
+
+        -----1,2---3,4-----|
+        [   starmap(add)   ]
+        -----3-----7-------|
+
+    Example:
+        >>> rs.ops.starmap(lambda x, y: x + y)
+
+    Source:
+        An Observable or a MuxObservable
+
+    Args:
+        mapper: A transform function to invoke with unpacked elements
+            as arguments.
+
+    Returns:
+        An observable containing the results of
+        invoking the mapper function with unpacked items of the
+        source observable.
+    """
     return map(lambda i: mapper(*i))
