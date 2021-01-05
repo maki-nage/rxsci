62d6d7fe4e5abc2f9ecd6687070f4d84bbea26ef
Romain Picard <romain.picard@oakbits.com>
2021-01-05 13:31:57 +0100

    count: add doc

--- rxsci/operators/count.py
+++ rxsci/operators/count.py
@@ -2,4 +2,28 @@ from .scan import scan
 
 
 def count(reduce=False):
+    """Counts the number of items emitted in the source Observable.
+
+    .. marble::
+        :alt: count
+
+        --4--1--2--1--|
+        [     count()      ]
+        --1--2--3--4--|
+
+    .. marble::
+        :alt: count
+
+        --4--1--2--1--|
+        [count(reduce=True)]
+        --------------4-|
+
+    Args:
+        reduce: [Optional] Emit an item for each source item when reduce is
+            False, otherwise emits a single item on completion.
+
+    Returns:
+        An observable emitting the number of source items that have been
+        emitted.
+    """
     return scan(lambda acc, i: acc + 1, 0, reduce=reduce)
