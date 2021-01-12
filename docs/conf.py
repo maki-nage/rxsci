9d1f5c7f618869145373f2bbd4d4a97a638e0f24
Romain Picard <romain.picard@oakbits.com>
2021-01-12 13:51:00 +0100

    Bump version: 0.9.0 → 0.10.0

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'R. Picard'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.9.0'
+release = '0.10.0'
 
 
 # -- General configuration ---------------------------------------------------
