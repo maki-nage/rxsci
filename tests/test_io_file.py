dfb5941b2c4a3ca4b97b39c3d230bc122b32b650
Romain Picard <romain.picard@oakbits.com>
2022-03-21 13:57:00 +0100

    ci: fix io tests

--- tests/test_io_file.py
+++ tests/test_io_file.py
@@ -1,20 +1,28 @@
+import os
 import tempfile
 import rxsci.io.file as file
 
+
 def test_read_text():
-    with tempfile.NamedTemporaryFile() as f:
-        f.write(b'Hello world!')
-        f.flush()
-        data = file.read(f.name).run()
+    with tempfile.TemporaryDirectory() as d:
+        f_name = os.path.join(d, "test")
+        with open(f_name, mode="wb") as f:
+            f.write(b'Hello world!')
+            f.flush()
+
+        data = file.read(f_name).run()
         assert data == 'Hello world!'
 
 
 def test_read_binary():
-    with tempfile.NamedTemporaryFile() as f:
-        f.write(b'Hello world!')
-        f.flush()
-        data = file.read(f.name, mode='rb').run()
+    with tempfile.TemporaryDirectory() as d:
+        f_name = os.path.join(d, "test")
+        with open(f_name, mode="wb") as f:
+            f.write(b'Hello world!')
+            f.flush()
+        data = file.read(f_name, mode='rb').run()
         assert data == b'Hello world!'
+
 
 def test_read_binary_with_size():
     actual_data = []
@@ -22,10 +30,13 @@ def test_read_binary_with_size():
     def on_next(i):
         actual_data.append(i)
 
-    with tempfile.NamedTemporaryFile() as f:
-        f.write(b'Hello world!')
-        f.flush()
-        data = file.read(f.name, mode='rb', size=5).subscribe(
+    with tempfile.TemporaryDirectory() as d:
+        f_name = os.path.join(d, "test")
+        with open(f_name, mode="wb") as f:
+            f.write(b'Hello world!')
+            f.flush()
+
+        file.read(f_name, mode='rb', size=5).subscribe(
             on_next=on_next
         )
 
