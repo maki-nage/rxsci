51b26afdd0d5efd669417650b9cd0d1b98987c94
Romain Picard <romain.picard@oakbits.com>
2024-09-23 21:55:31 +0200

    run: use CurrentThreadScheduler instead of NewThreadScheduler

--- rxsci/run.py
+++ rxsci/run.py
@@ -1,8 +1,38 @@
+import threading
 import rx
+from rx.scheduler import CurrentThreadScheduler
 
 
 def run(pipe):
-    try:
-        return pipe.run()
-    except rx.internal.SequenceContainsNoElementsError:
-        pass
+    exception = None
+    latch = threading.Event()
+    has_result = False
+    result = None
+    done = False
+
+    def on_next(value):
+        nonlocal result, has_result
+        result = value
+        has_result = True
+
+    def on_error(error):
+        nonlocal exception, done
+
+        exception = error
+        done = True
+        latch.set()
+
+    def on_completed() -> None:
+        nonlocal done
+        done = True
+        latch.set()
+
+    pipe.subscribe(on_next, on_error, on_completed, scheduler=CurrentThreadScheduler())
+
+    while not done:
+        latch.wait()
+
+    if exception:
+        raise exception
+
+    return result
